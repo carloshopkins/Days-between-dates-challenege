1145e9eebd2eae7c8da0839d15d383a6311efc43
Carlos Hopkins <Carlos Hopkins>
2017-03-26 08:58:25 -0400

    Add project files.

---
+++ ChallengeDaysBetweenDates/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeDaysBetweenDates.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+            Choose one date:</p>
+        <p>
+            <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Choose a second date:</p>
+        <p>
+            <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Ok" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            //Set the selected calendars dates to usable DateTime values
+            DateTime cal1 = Calendar1.SelectedDate;
+            DateTime cal2 = Calendar2.SelectedDate;
+
+            //Determine the number of days between the selected dates
+            TimeSpan calSpan = cal2.Subtract(cal1);
+
+            //If the number of days between the days is a negative value
+            //This will change it to a positive value i.e. the user selected a later date on the first calendar
+            int daySpanTest = Int32.Parse(calSpan.Days.ToString());
+            if (daySpanTest < 0)
+            {
+                daySpanTest *= -1;
+            }
+
+            //Prints the results to the screen below the "Ok" button
+            resultLabel.Text = "There are " + daySpanTest + " days between " + cal1.ToShortDateString() + " & " + cal2.ToShortDateString();
+            
+        }
+    }
+}
